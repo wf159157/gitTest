b54c86e81986265cc50b047c0ef97924a474a174
wf159157 <1591578312@qq.com>
2020-04-28 23:15:18 +0800

    添加代码

--- src/service/User.java
+++ src/service/User.java
@@ -1,4 +1,30 @@
 package service;
 
 public class User {
+    private Integer id;
+    private String userName;
+
+    public User(Integer id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }

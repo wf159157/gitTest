097480a3c1ff16c4f7a7db858e89e76aae5524c1
wf159157 <1591578312@qq.com>
2020-04-28 23:29:50 +0800

    添加cat

---
+++ src/service/Cat.java
@@ -0,0 +1,11 @@
+package service;
+
+public class Cat {
+    private String name;
+    private Integer age;
+
+    public Cat(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}

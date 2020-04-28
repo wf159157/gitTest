6c500cae8fc57611a6761934b99a7f5c4e573a14
wf159157 <1591578312@qq.com>
2020-04-28 23:33:07 +0800

    修改cat

--- src/service/Cat.java
+++ src/service/Cat.java
@@ -8,4 +8,20 @@ public class Cat {
         this.name = name;
         this.age = age;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
